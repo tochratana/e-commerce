ea167f60145c06217b4caf77bda66c11e501a775
RML-Cambo <ltomusic69@gmail.com>
2025-06-13 10:02:22 +0700

    fix: connect to database

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,7 +5,7 @@ import view.UserUI;
 public class Application {
     public static void main(String[] args) {
         UserUI.home();
-        ProductServer productServer = new ProductServer();
-        productServer.start();
+//        ProductServer productServer = new ProductServer();
+//        productServer.start();
     }
 }
--- src/main/java/utils/DatabaseConnection.java
+++ src/main/java/utils/DatabaseConnection.java
@@ -4,9 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseConnection {
-    private static final String databaseUrl  ="jdbc:postgresql://localhost:5432/postgres";
-    private static final String userName = "postgres";
-    private static final String password = "qwer";
+    private static final String databaseUrl  ="jdbc:postgresql://istad_e_commerce:NK4DihgbjX8WW8LkA5E1I1Z8Mt9Pf7HG" +
+            "@dpg-d12n5715pdvs73ctrn00-a.oregon-postgres.render.com/e_commerce_heqn";
+    private static final String userName = "istad_e_commerce";
+    private static final String password = "NK4DihgbjX8WW8LkA5E1I1Z8Mt9Pf7HG";
     public static Connection getConnection(){
         try{
             return DriverManager.getConnection(databaseUrl, userName, password);
